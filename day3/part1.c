d6b9851a19832c5addfefcd557c148ce2e75e9a8
Ruslan Kovtun <koutoftimer@gmail.com>
2025-12-06 23:19:00 +0200

    day 3 part 1

---
+++ day3/part1.c
@@ -0,0 +1,36 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+static_assert(sizeof(long) == 8);
+
+int
+main(int argc, char *argv[])
+{
+        assert(argc == 2);
+        FILE *f = fopen(argv[1], "r");
+        assert(f);
+
+        char line[200] = {0};
+        long sum       = 0;
+        while (fscanf(f, "%s", line) != EOF) {
+                int len       = strlen(line);
+                long tail_max = line[len - 1];
+                long max      = INT64_MIN;
+                for (int i = len - 2; i >= 0; --i) {
+                        long cur = line[i] * 10 + tail_max - '0' * 11;
+                        if (cur > max) max = cur;
+                        if (line[i] > tail_max) tail_max = line[i];
+                }
+                sum += max;
+        }
+
+        printf("sum: %ld\n", sum);
+        if (strcmp(argv[1], "test") == 0) {
+                assert(sum == 357);
+        }
+        if (strcmp(argv[1], "input") == 0) {
+        }
+}
